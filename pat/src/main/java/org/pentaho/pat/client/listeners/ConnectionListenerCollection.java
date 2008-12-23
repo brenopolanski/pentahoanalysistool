c475d78aee96ac3c0d52cf637c42b4cc03321793
tom@wamonline.org.uk <tom@wamonline.org.uk@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2008-12-23 11:13:56 +0000

    Patched POM.xml and Connection working(I think)

--- pat/src/main/java/org/pentaho/pat/client/listeners/ConnectionListenerCollection.java
+++ pat/src/main/java/org/pentaho/pat/client/listeners/ConnectionListenerCollection.java
@@ -3,12 +3,14 @@ package org.pentaho.pat.client.listeners;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.pentaho.pat.client.panels.services.ControlBar;
+
 import com.google.gwt.user.client.ui.Widget;
 
 /**
  * A helper class for implementers of the SourcesConnectionEvents interface. This
  * subclass of {@link ArrayList} assumes that all objects added to it will be of
- * type {@link @link org.pentaho.halogen.client.ConnectionListener}.
+ * type {@link @link org.pentaho.pat.client.ConnectionListener}.
  */
 public class ConnectionListenerCollection extends ArrayList {
 
@@ -30,4 +32,5 @@ public class ConnectionListenerCollection extends ArrayList {
       listener.onConnectionBroken(sender);
     }    
   }
+
 }
--- pat/src/main/java/org/pentaho/pat/client/panels/services/ControlBar.java
+++ pat/src/main/java/org/pentaho/pat/client/panels/services/ControlBar.java
@@ -1,16 +1,24 @@
 package org.pentaho.pat.client.panels.services;
 
+import org.pentaho.pat.client.events.SourcesConnectionEvents;
+import org.pentaho.pat.client.listeners.ConnectionListener;
+import org.pentaho.pat.client.listeners.ConnectionListenerCollection;
+import org.pentaho.pat.client.panels.ControlBarPanel;
 import org.pentaho.pat.client.util.GuidFactory;
+import org.pentaho.pat.client.util.MessageFactory;
 import org.pentaho.pat.client.util.ServiceFactory;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
 import com.gwtext.client.data.ArrayReader;
 import com.gwtext.client.data.FieldDef;
 import com.gwtext.client.data.MemoryProxy;
 import com.gwtext.client.data.RecordDef;
 import com.gwtext.client.data.Store;
 import com.gwtext.client.data.StringFieldDef;
+import com.gwtext.client.widgets.Toolbar;
 
-public class ControlBar {
+public class ControlBar implements SourcesConnectionEvents {
 	
 	private static Store store;
 	private static MemoryProxy proxy;
@@ -24,9 +32,24 @@ public class ControlBar {
 	public static final String AXIS_PAGES = "PAGES"; //$NON-NLS-1$
 	public static final String AXIS_CHAPTERS = "CHAPTERS"; //$NON-NLS-1$
 	public static final String AXIS_SECTIONS = "SECTIONS"; //$NON-NLS-1$
-	
+	//Need deleting once connection dialog is written...
+	static String queryTypeGroup = "QUERY_TYPE"; //$NON-NLS-1$
+	boolean connectionEstablished = false;
+	ConnectionListenerCollection connectionListeners;
+	  
+
+	  
 	public ControlBar() {
-	
+		 super();
+
+		 init();
+
+	}
+
+
+	private void init() {
+		// TODO Auto-generated method stub
+		
 	}
 
 
@@ -59,7 +82,7 @@ public class ControlBar {
 		return list;
 	}
 
-	public static void setCube(String boxText){
+	public void setCube(String boxText){
 	
 		ServiceFactory.getInstance().setCube(
 				boxText,
@@ -84,4 +107,87 @@ public class ControlBar {
 	}
 	
 	
+	
+	
+	
+	  public boolean isConnectionEstablished() {
+		    return connectionEstablished;
+		  }
+
+		  public void setConnectionEstablished(boolean connectionEstablished) {
+		    this.connectionEstablished = connectionEstablished;
+		    
+		  }
+		  
+		  
+	public void connect(String connectionStr) {
+		
+	    if (!isConnectionEstablished()) {
+	      ServiceFactory.getInstance().connect(connectionStr, GuidFactory.getGuid(), new AsyncCallback() {
+	        public void onSuccess(Object result) {
+	          Boolean booleanResult = (Boolean)result;
+	          if (booleanResult.booleanValue()) {
+	            setConnectionEstablished(true);
+	           // connectionListeners.fireConnectionMade();
+	          } else {
+	            setConnectionEstablished(false);
+	           // connectionListeners.fireConnectionBroken();
+	          }
+	         // conStr= isConnectionEstablished() ? MessageFactory.getInstance().disconnect() : MessageFactory.getInstance().connect();
+	          
+	        }
+	        public void onFailure(Throwable caught) {
+	          System.out.println(caught.getLocalizedMessage());
+	        	Window.alert(MessageFactory.getInstance().no_connection_param(caught.getLocalizedMessage()));
+	          setConnectionEstablished(false);
+	         // conStr= (isConnectionEstablished() ? MessageFactory.getInstance().disconnect() : MessageFactory.getInstance().connect());
+	        }      
+	      });
+	      
+	    }
+	    
+	    
+	  }
+	  
+	  public void disconnect() {
+	    if (isConnectionEstablished()) {
+	      ServiceFactory.getInstance().disconnect(GuidFactory.getGuid(), new AsyncCallback<Object>() {
+	        public void onFailure(Throwable caught) {
+	          Window.alert(MessageFactory.getInstance().no_connection_param(caught.getLocalizedMessage()));
+	          setConnectionEstablished(false);
+	          //connectionListeners.fireConnectionBroken(ControlBarPanel.this);
+	         // conStr = (isConnectionEstablished() ? MessageFactory.getInstance().disconnect() : MessageFactory.getInstance().connect());
+	        }
+	        public void onSuccess(Object result) {
+	          setConnectionEstablished(false);
+	          //connectionListeners.fireConnectionBroken(ControlBar.this);
+	         // conStr = (isConnectionEstablished() ? MessageFactory.getInstance().disconnect() : MessageFactory.getInstance().connect());
+	        }       
+	      });
+	    }
+	    
+	  }
+
+	  /* (non-Javadoc)
+	   * @see org.pentaho.halogen.client.listeners.SourcesConnectionEvents#addConnectionListener(org.pentaho.halogen.client.listeners.ConnectionListener)
+	   */
+	  public void addConnectionListener(ConnectionListener listener) {
+	    if (connectionListeners == null) {
+	      connectionListeners = new ConnectionListenerCollection();
+	    }
+	    connectionListeners.add(listener);
+	  }
+
+	  /* (non-Javadoc)
+	   * @see org.pentaho.halogen.client.listeners.SourcesConnectionEvents#removeClickListener(org.pentaho.halogen.client.listeners.ConnectionListener)
+	   */
+	  public void removeClickListener(ConnectionListener listener) {
+	    if (connectionListeners != null) {
+	      connectionListeners.remove(listener);
+	    }
+	  }
+
+
+
+
 }
