c62493647baffe1330160d55c55e4b8d005efee5
lucboudreau <lucboudreau@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-01-21 20:44:54 +0000

    Cubes discovery implemented

--- pat/src/main/java/org/pentaho/pat/server/services/impl/DiscoveryServiceImpl.java
+++ pat/src/main/java/org/pentaho/pat/server/services/impl/DiscoveryServiceImpl.java
@@ -1,9 +1,15 @@
 package org.pentaho.pat.server.services.impl;
 
+import java.security.InvalidKeyException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.list.TreeList;
 import org.olap4j.Axis;
+import org.olap4j.OlapConnection;
+import org.olap4j.OlapException;
+import org.olap4j.metadata.Cube;
+import org.olap4j.metadata.NamedList;
 import org.pentaho.pat.server.data.ConnectionManager;
 import org.pentaho.pat.server.services.DiscoveryService;
 import org.pentaho.pat.server.services.SessionService;
@@ -14,11 +20,40 @@ public class DiscoveryServiceImpl implements DiscoveryService {
 	
 	private ConnectionManager connectionManager;
 	
+	
+	
 	public List<String> getCubes(String guid) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			List<String> list = new ArrayList<String>();
+			
+			OlapConnection conn = this.connectionManager.getConnection(guid);
+			
+			if (conn == null)
+				return list;
+			
+			NamedList<Cube> cubes = conn.getSchema().getCubes();
+			
+			for (int i = 0; i < cubes.size(); i++) {
+				list.add(cubes.get(i).getName());
+			}
+			return list;
+			
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OlapException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
+	
+	
+	
 	public List<String> getDimensions(Axis axis, String guid) {
 		// TODO Auto-generated method stub
 		return null;
