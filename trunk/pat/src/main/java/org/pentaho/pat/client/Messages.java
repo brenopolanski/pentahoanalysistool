45adc44d60b03c99f3d2333cd9d4e2f734ab372f
tom@wamonline.org.uk <tom@wamonline.org.uk@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-01-23 11:18:18 +0000

    Changed the layout so it resizes dynamically, added a few more bits to the messages

--- trunk/pat/src/main/java/org/pentaho/pat/client/Messages.java
+++ trunk/pat/src/main/java/org/pentaho/pat/client/Messages.java
@@ -1,449 +1,475 @@
 package org.pentaho.pat.client;
 
-
 /**
- * Interface to represent the messages contained in resource  bundle:
- * 	/Users/wseyler/Documents/workspace-trunk/halogen/src/org/pentaho/halogen/client/Messages.properties'.
+ * Interface to represent the messages contained in resource bundle:
+ * /Users/wseyler
+ * /Documents/workspace-trunk/halogen/src/org/pentaho/halogen/client
+ * /Messages.properties'.
  */
 public interface Messages extends com.google.gwt.i18n.client.Messages {
-  
-  /**
-   * Translated "Connection String:".
-   * 
-   * @return translated "Connection String:"
-   *  connection_string
-   */
-  String connection_string();
 
-  /**
-   * Translated "Row Dimensions".
-   * 
-   * @return translated "Row Dimensions"
-   *  row_dimensions
-   */
-  String row_dimensions();
+	/**
+	 * Translated "Connection String:".
+	 * 
+	 * @return translated "Connection String:" 
+	 * connection_string
+	 */
+	String connection_string();
 
-  /**
-   * Translated "Chart Height:".
-   * 
-   * @return translated "Chart Height:"
-   *  chart_height
-   */
-  String chart_height();
+	/**
+	 * Translated "Row Dimensions".
+	 * 
+	 * @return translated "Row Dimensions" 
+	 * row_dimensions
+	 */
+	String row_dimensions();
 
-  /**
-   * Translated "Top".
-   * 
-   * @return translated "Top"
-   *  top
-   */
-  String top();
+	/**
+	 * Translated "Chart Height:".
+	 * 
+	 * @return translated "Chart Height:" 
+	 * chart_height
+	 */
+	String chart_height();
 
-  /**
-   * Translated "OK".
-   * 
-   * @return translated "OK"
-   *  ok
-   */
-  String ok();
+	/**
+	 * Translated "Top".
+	 * 
+	 * @return translated "Top" 
+	 * top
+	 */
+	String top();
 
-  /**
-   * Translated "No Data Available.  Query may be invalid.".
-   * 
-   * @return translated "No Data Available.  Query may be invalid."
-   *  no_data
-   */
-  String no_data();
+	/**
+	 * Translated "OK".
+	 * 
+	 * @return translated "OK" 
+	 * ok
+	 */
+	String ok();
 
-  /**
-   * Translated "Chart Width:".
-   * 
-   * @return translated "Chart Width:"
-   *  chart_width
-   */
-  String chart_width();
+	/**
+	 * Translated "No Data Available.  Query may be invalid.".
+	 * 
+	 * @return translated "No Data Available.  Query may be invalid." 
+	 * no_data
+	 */
+	String no_data();
 
-  /**
-   * Translated "Selection could not be cleared: {0}".
-   * 
-   * @return translated "Selection could not be cleared: {0}"
-   *  no_selection_cleared
-   */
-  String no_selection_cleared(String arg0);
+	/**
+	 * Translated "Chart Width:".
+	 * 
+	 * @return translated "Chart Width:" 
+	 * chart_width
+	 */
+	String chart_width();
 
-  /**
-   * Translated "Show Parents".
-   * 
-   * @return translated "Show Parents"
-   *  show_parents
-   */
-  String show_parents();
+	/**
+	 * Translated "Selection could not be cleared: {0}".
+	 * 
+	 * @return translated "Selection could not be cleared: {0}"
+	 *         no_selection_cleared
+	 */
+	String no_selection_cleared(String arg0);
 
-  /**
-   * Translated "Location:".
-   * 
-   * @return translated "Location:"
-   *  location
-   */
-  String location();
+	/**
+	 * Translated "Show Parents".
+	 * 
+	 * @return translated "Show Parents" 
+	 * show_parents
+	 */
+	String show_parents();
 
-  /**
-   * Translated "Disconnect".
-   * 
-   * @return translated "Disconnect"
-   *  disconnect
-   */
-  String disconnect();
+	/**
+	 * Translated "Location:".
+	 * 
+	 * @return translated "Location:" 
+	 * location
+	 */
+	String location();
 
-  /**
-   * Translated "Move To Filter".
-   * 
-   * @return translated "Move To Filter"
-   *  move_to_filter
-   */
-  String move_to_filter();
+	/**
+	 * Translated "Disconnect".
+	 * 
+	 * @return translated "Disconnect" 
+	 * disconnect
+	 */
+	String disconnect();
 
-  /**
-   * Translated "Cancel".
-   * 
-   * @return translated "Cancel"
-   *  cancel
-   */
-  String cancel();
+	/**
+	 * Translated "Move To Filter".
+	 * 
+	 * @return translated "Move To Filter" 
+	 * move_to_filter
+	 */
+	String move_to_filter();
 
-  /**
-   * Translated "Connection".
-   * 
-   * @return translated "Connection"
-   *  connection
-   */
-  String connection();
+	/**
+	 * Translated "Cancel".
+	 * 
+	 * @return translated "Cancel" 
+	 * cancel
+	 */
+	String cancel();
 
-  /**
-   * Translated "OLAP Chart".
-   * 
-   * @return translated "OLAP Chart"
-   *  olap_chart
-   */
-  String olap_chart();
+	/**
+	 * Translated "Connection".
+	 * 
+	 * @return translated "Connection" 
+	 * connection
+	 */
+	String connection();
 
-  /**
-   * Translated "Siblings".
-   * 
-   * @return translated "Siblings"
-   *  siblings
-   */
-  String siblings();
+	/**
+	 * Translated "OLAP Chart".
+	 * 
+	 * @return translated "OLAP Chart" 
+	 * olap_chart
+	 */
+	String olap_chart();
 
-  /**
-   * Translated "Report".
-   * 
-   * @return translated "Report"
-   *  report
-   */
-  String report();
+	/**
+	 * Translated "Siblings".
+	 * 
+	 * @return translated "Siblings" 
+	 * siblings
+	 */
+	String siblings();
 
-  /**
-   * Translated "null".
-   * 
-   * @return translated "null"
-   *  null_value
-   */
-  String null_value();
+	/**
+	 * Translated "Report".
+	 * 
+	 * @return translated "Report" 
+	 * report
+	 */
+	String report();
 
-  /**
-   * Translated "Use MDX".
-   * 
-   * @return translated "Use MDX"
-   *  use_mdx
-   */
-  String use_mdx();
+	/**
+	 * Translated "null".
+	 * 
+	 * @return translated "null" 
+	 * null_value
+	 */
+	String null_value();
 
-  /**
-   * Translated "Bottom".
-   * 
-   * @return translated "Bottom"
-   *  bottom
-   */
-  String bottom();
+	/**
+	 * Translated "Use MDX".
+	 * 
+	 * @return translated "Use MDX" 
+	 * use_mdx
+	 */
+	String use_mdx();
 
-  /**
-   * Translated "Connect".
-   * 
-   * @return translated "Connect"
-   *  connect
-   */
-  String connect();
+	/**
+	 * Translated "Bottom".
+	 * 
+	 * @return translated "Bottom" 
+	 * bottom
+	 */
+	String bottom();
 
-  /**
-   * Translated "Move To Column".
-   * 
-   * @return translated "Move To Column"
-   *  move_to_column
-   */
-  String move_to_column();
+	/**
+	 * Translated "Connect".
+	 * 
+	 * @return translated "Connect" 
+	 * connect
+	 */
+	String connect();
 
-  /**
-   * Translated "Select Cube".
-   * 
-   * @return translated "Select Cube"
-   *  select_cube
-   */
-  String select_cube();
+	/**
+	 * Translated "Move To Column".
+	 * 
+	 * @return translated "Move To Column" 
+	 * move_to_column
+	 */
+	String move_to_column();
 
-  /**
-   * Translated "Chart".
-   * 
-   * @return translated "Chart"
-   *  chart
-   */
-  String chart();
+	/**
+	 * Translated "Select Cube".
+	 * 
+	 * @return translated "Select Cube"
+	 * select_cube
+	 */
+	String select_cube();
 
-  /**
-   * Translated "Next".
-   * 
-   * @return translated "Next"
-   *  next
-   */
-  String next();
+	/**
+	 * Translated "Chart".
+	 * 
+	 * @return translated "Chart" 
+	 * chart
+	 */
+	String chart();
 
-  /**
-   * Translated "Execute MDX".
-   * 
-   * @return translated "Execute MDX"
-   *  execute_mdx
-   */
-  String execute_mdx();
+	/**
+	 * Translated "Next".
+	 * 
+	 * @return translated "Next" 
+	 * next
+	 */
+	String next();
 
-  /**
-   * Translated "Member".
-   * 
-   * @return translated "Member"
-   *  member
-   */
-  String member();
+	/**
+	 * Translated "Execute MDX".
+	 * 
+	 * @return translated "Execute MDX" 
+	 * execute_mdx
+	 */
+	String execute_mdx();
 
-  /**
-   * Translated "Unable to get data from server: {0}".
-   * 
-   * @return translated "Unable to get data from server: {0}"
-   *  no_server_data
-   */
-  String no_server_data(String arg0);
+	/**
+	 * Translated "Member".
+	 * 
+	 * @return translated "Member" 
+	 * member
+	 */
+	String member();
 
-  /**
-   * Translated "Grid".
-   * 
-   * @return translated "Grid"
-   *  grid
-   */
-  String grid();
+	/**
+	 * Translated "Unable to get data from server: {0}".
+	 * 
+	 * @return translated "Unable to get data from server: {0}" 
+	 * no_server_data
+	 */
+	String no_server_data(String arg0);
 
-  /**
-   * Translated "Hide Parents".
-   * 
-   * @return translated "Hide Parents"
-   *  hide_parents
-   */
-  String hide_parents();
+	/**
+	 * Translated "Grid".
+	 * 
+	 * @return translated "Grid" 
+	 * grid
+	 */
+	String grid();
 
-  /**
-   * Translated "Left".
-   * 
-   * @return translated "Left"
-   *  left
-   */
-  String left();
+	/**
+	 * Translated "Hide Parents".
+	 * 
+	 * @return translated "Hide Parents" 
+	 * hide_parents
+	 */
+	String hide_parents();
 
-  /**
-   * Translated "Selections".
-   * 
-   * @return translated "Selections"
-   *  selections
-   */
-  String selections();
+	/**
+	 * Translated "Left".
+	 * 
+	 * @return translated "Left" 
+	 * left
+	 */
+	String left();
 
-  /**
-   * Translated "Execute Query Model".
-   * 
-   * @return translated "Execute Query Model"
-   *  execute_query
-   */
-  String execute_query();
+	/**
+	 * Translated "Selections".
+	 * 
+	 * @return translated "Selections" 
+	 * selections
+	 */
+	String selections();
 
-  /**
-   * Translated "Group Headers".
-   * 
-   * @return translated "Group Headers"
-   *  group_headers
-   */
-  String group_headers();
+	/**
+	 * Translated "Execute Query Model".
+	 * 
+	 * @return translated "Execute Query Model" 
+	 * execute_query
+	 */
+	String execute_query();
 
-  /**
-   * Translated "Clear".
-   * 
-   * @return translated "Clear"
-   *  clear_selections
-   */
-  String clear_selections();
+	/**
+	 * Translated "Group Headers".
+	 * 
+	 * @return translated "Group Headers" 
+	 * group_headers
+	 */
+	String group_headers();
 
-  /**
-   * Translated "Connection could not be established.  Either you did not press the connect button, or there is a problem with your connection string.".
-   * 
-   * @return translated "Connection could not be established.  Either you did not press the connect button, or there is a problem with your connection string."
-   *  no_connection
-   */
-  String no_connection();
+	/**
+	 * Translated "Clear".
+	 * 
+	 * @return translated "Clear" 
+	 * clear_selections
+	 */
+	String clear_selections();
 
-  /**
-   * Translated "Chart Title:".
-   * 
-   * @return translated "Chart Title:"
-   *  chart_title
-   */
-  String chart_title();
+	/**
+	 * Translated"Connection could not be established.  Either you did not press the connect button, or there is a problem with your connection string."
+	 * .
+	 * 
+	 * @return translated"Connection could not be established.  Either you did not press the connect button, or there is a problem with your connection string."
+	 *         no_connection
+	 */
+	String no_connection();
 
-  /**
-   * Translated "Column Dimensions".
-   * 
-   * @return translated "Column Dimensions"
-   *  column_dimensions
-   */
-  String column_dimensions();
+	/**
+	 * Translated "Chart Title:".
+	 * 
+	 * @return translated "Chart Title:" 
+	 * chart_title
+	 */
+	String chart_title();
 
-  /**
-   * Translated "Right".
-   * 
-   * @return translated "Right"
-   *  right
-   */
-  String right();
+	/**
+	 * Translated "Column Dimensions".
+	 * 
+	 * @return translated "Column Dimensions" 
+	 * column_dimensions
+	 */
+	String column_dimensions();
 
-  /**
-   * Translated "MDX Query:".
-   * 
-   * @return translated "MDX Query:"
-   *  mdx_query
-   */
-  String mdx_query();
+	/**
+	 * Translated "Right".
+	 * 
+	 * @return translated "Right" 
+	 * right
+	 */
+	String right();
 
-  /**
-   * Translated "Move To Row".
-   * 
-   * @return translated "Move To Row"
-   *  move_to_row
-   */
-  String move_to_row();
+	/**
+	 * Translated "MDX Query:".
+	 * 
+	 * @return translated "MDX Query:" 
+	 * mdx_query
+	 */
+	String mdx_query();
 
-  /**
-   * Translated "Filter Dimensions".
-   * 
-   * @return translated "Filter Dimensions"
-   *  filter_dimensions
-   */
-  String filter_dimensions();
+	/**
+	 * Translated "Move To Row".
+	 * 
+	 * @return translated "Move To Row" 
+	 * move_to_row
+	 */
+	String move_to_row();
 
-  /**
-   * Translated "Welcome.  The current time is {0}.".
-   * 
-   * @return translated "Welcome.  The current time is {0}."
-   *  welcome
-   */
-  String welcome(String arg0);
+	/**
+	 * Translated "Filter Dimensions".
+	 * 
+	 * @return translated "Filter Dimensions" 
+	 * filter_dimensions
+	 */
+	String filter_dimensions();
 
-  /**
-   * Translated "Swap Axis".
-   * 
-   * @return translated "Swap Axis"
-   *  swap_axis
-   */
-  String swap_axis();
+	/**
+	 * Translated "Welcome.  The current time is {0}.".
+	 * 
+	 * @return translated "Welcome.  The current time is {0}." 
+	 * welcome
+	 */
+	String welcome(String arg0);
 
-  /**
-   * Translated "Dimensions".
-   * 
-   * @return translated "Dimensions"
-   *  dimensions
-   */
-  String dimensions();
+	/**
+	 * Translated "Swap Axis".
+	 * 
+	 * @return translated "Swap Axis" 
+	 * swap_axis
+	 */
+	String swap_axis();
 
-  /**
-   * Translated "Include Children".
-   * 
-   * @return translated "Include Children"
-   *  include_children
-   */
-  String include_children();
+	/**
+	 * Translated "Dimensions".
+	 * 
+	 * @return translated "Dimensions" 
+	 * dimensions
+	 */
+	String dimensions();
 
-  /**
-   * Translated "Ungroup Headers".
-   * 
-   * @return translated "Ungroup Headers"
-   *  ungroup_headers
-   */
-  String ungroup_headers();
+	/**
+	 * Translated "Include Children".
+	 * 
+	 * @return translated "Include Children" 
+	 * include_children
+	 */
+	String include_children();
 
-  /**
-   * Translated "Connection could not be established: {0}".
-   * 
-   * @return translated "Connection could not be established: {0}"
-   *  no_connection_param
-   */
-  String no_connection_param(String arg0);
+	/**
+	 * Translated "Ungroup Headers".
+	 * 
+	 * @return translated "Ungroup Headers" 
+	 * ungroup_headers
+	 */
+	String ungroup_headers();
 
-  /**
-   * Translated "Use Schema Navigator".
-   * 
-   * @return translated "Use Schema Navigator"
-   *  use_schema_nav
-   */
-  String use_schema_nav();
+	/**
+	 * Translated "Connection could not be established: {0}".
+	 * 
+	 * @return translated "Connection could not be established: {0}"
+	 *         no_connection_param
+	 */
+	String no_connection_param(String arg0);
 
-  /**
-   * Translated "Selection mode could not be set: {0}".
-   * 
-   * @return translated "Selection mode could not be set: {0}"
-   *  no_selection_set
-   */
-  String no_selection_set(String arg0);
+	/**
+	 * Translated "Use Schema Navigator".
+	 * 
+	 * @return translated "Use Schema Navigator" 
+	 * use_schema_nav
+	 */
+	String use_schema_nav();
 
-  /**
-   * Translated "Visible:".
-   * 
-   * @return translated "Visible:"
-   *  visible
-   */
-  String visible();
+	/**
+	 * Translated "Selection mode could not be set: {0}".
+	 * 
+	 * @return translated "Selection mode could not be set: {0}"
+	 *         no_selection_set
+	 */
+	String no_selection_set(String arg0);
 
-  /**
-   * Translated "Chart Properties...".
-   * 
-   * @return translated "Chart Properties..."
-   *  chart_properties
-   */
-  String chart_properties();
+	/**
+	 * Translated "Visible:".
+	 * 
+	 * @return translated "Visible:" 
+	 * visible
+	 */
+	String visible();
 
-  /**
-   * Translated "Children".
-   * 
-   * @return translated "Children"
-   *  children
-   */
-  String children();
-  
-  /**
-   * Translated "Cube".
-   * 
-   * @return translated "Cube"
-   *  cube
-   */
-  String cube();
-  
-  /**
-   * Translated "File".
-   * 
-   * @return translated "File"
-   *  file
-   */
-  String file();
+	/**
+	 * Translated "Chart Properties...".
+	 * 
+	 * @return translated "Chart Properties..." 
+	 * chart_properties
+	 */
+	String chart_properties();
+
+	/**
+	 * Translated "Children".
+	 * 
+	 * @return translated "Children" 
+	 * children
+	 */
+	String children();
+
+	/**
+	 * Translated "Cube".
+	 * 
+	 * @return translated "Cube" 
+	 * cube
+	 */
+	String cube();
+
+	/**
+	 * Translated "File".
+	 * 
+	 * @return translated "File" 
+	 * file
+	 */
+	String file();
+
+	/**
+	 * Translated "Dimension Panel".
+	 * 
+	 * @return translated "Dimension Panel" 
+	 * dimensionPanel
+	 */
+	String dimensionPanel();
+
+	/**
+	 * Translated "MDX/Filter Panel".
+	 * 
+	 * @return translated "MDX/Filter Panel" 
+	 * mdxPanel
+	 */
+	String mdxPanel();
+
+	/**
+	 * Translated "Drill Panel".
+	 * 
+	 * @return translated "Drill Panel" 
+	 * drillPanel
+	 */
+	String drillPanel();
 }
