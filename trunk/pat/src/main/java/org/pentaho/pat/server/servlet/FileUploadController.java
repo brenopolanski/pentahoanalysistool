b8da617e96761ba5ddf6bf867f8c26905b433eb1
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-02-04 15:36:46 +0000

    change schema_temp file path still needs some work

--- trunk/pat/src/main/java/org/pentaho/pat/server/servlet/FileUploadController.java
+++ trunk/pat/src/main/java/org/pentaho/pat/server/servlet/FileUploadController.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,11 +16,15 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import java.util.Iterator;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.metadata.commons.CommonsAttributes;
+import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class FileUploadController extends AbstractController {
+public class FileUploadController extends AbstractController implements ServletContextAware {
 
 	private File basedir;
 	
@@ -29,6 +34,7 @@ public class FileUploadController extends AbstractController {
 	public void setBasedir(File basedir) {
 		this.basedir = basedir;
 	}
+	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
 			HttpServletResponse arg1) throws Exception {
@@ -48,24 +54,27 @@ public class FileUploadController extends AbstractController {
 			FileItem item = (FileItem)it.next();
 			if(!item.isFormField()) {
 			byte[] data = item.get();
-			
+
 			fn = "" + new Date().getTime();
-			if (basedir.mkdir()) System.out.print("Created Schema_temp Directory\n");
-			FileWriter fw = new FileWriter(new File(basedir,fn));
+			File f = basedir;
+			if (f.mkdir()) 
+				System.out.println("Created Directory: " + f.getAbsolutePath());
+			f = new File(f,fn);
+			FileWriter fw = new FileWriter(f);
 			BufferedWriter out = new BufferedWriter(fw);
 			
 			out.write(new String(data));
 			out.close();
 			//arg1.getOutputStream().println(fn);
-			System.out.print("Uploaded Schema file:" + fn);
-			arg1.getWriter().print(fn);
+			System.out.println("Uploaded Schema file:" + f.getAbsolutePath());
+			arg1.getWriter().print(f.toString());
 			}
 		}
 		arg1.setStatus(HttpServletResponse.SC_OK);
 		}
 		catch (Exception e)
 		{
-			System.out.println(e.getMessage());
+			System.out.println("ERROR:" + e.getMessage());
 			arg1.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		}
 		arg1.getWriter().flush();		
