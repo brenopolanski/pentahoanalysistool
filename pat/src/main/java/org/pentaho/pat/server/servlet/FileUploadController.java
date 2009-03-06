7e583cecd5c19ed62d61eada241bbcd092efbd3b
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-03-06 13:19:59 +0000

    fix schema upload (NOT WORKING,  missing schema_temp in target) modify pom (dependency, mondrian.xml fix)

--- pat/src/main/java/org/pentaho/pat/server/servlet/FileUploadController.java
+++ pat/src/main/java/org/pentaho/pat/server/servlet/FileUploadController.java
@@ -1,50 +1,88 @@
 package org.pentaho.pat.server.servlet;
-import java.io.File; 
-import java.io.IOException; 
-import org.springframework.stereotype.Controller; 
-import org.springframework.ui.ModelMap; 
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RequestMethod; 
-import org.springframework.web.bind.annotation.RequestParam; 
-import org.springframework.web.multipart.MultipartFile; 
-import org.springframework.web.servlet.ModelAndView; 
- 
-@Controller 
-public class FileUploadController { 
- 
-    private File basedir; 
+import java.io.File;
+import java.util.UUID;
 
-    public void setBasedir(File basedir) {
-        this.basedir = basedir;
-}
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-//        @SuppressWarnings("unchecked") 
-        @RequestMapping(value = "/schemaupload", method = RequestMethod.POST) 
-        public ModelAndView postUploadForm(@RequestParam(value = "map['file']") MultipartFile file) { 
-                ModelAndView mav = new ModelAndView(); 
-                try { 
-           file.transferTo(new File(this.basedir, "temp.test")); 
-        } catch (IllegalStateException e1) { 
-            // TODO Auto-generated catch block 
-            e1.printStackTrace(); 
-        } catch (IOException e1) { 
-            // TODO Auto-generated catch block 
-            e1.printStackTrace(); 
-        } 
-                
-        mav.setViewName("success"); 
-//       
-//        ModelMap map = mav.getModelMap(); 
-//                
-//                try { 
-//                        map.put("testObj", new String(file.getBytes()) + " " + file.getSize()); 
-//                } catch (IOException e) { 
-//                        e.printStackTrace(); 
-//                } 
-//                
-//                map.put("tasks", this.taskDao.list()); 
-//                
-                return mav; 
-        } 
+import org.apache.log4j.Logger;
+import org.pentaho.pat.server.beans.FileUploadBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractCommandController;
 
-} 
+public class FileUploadController extends AbstractCommandController implements
+ResourceLoaderAware, InitializingBean {
+  
+	      
+
+     Logger log = Logger.getLogger(this.getClass());
+     
+     private String basedir = null;
+     private File schemaDirectory = null;
+     private ResourceLoader resourceLoader;
+
+     public void setBasedir(String basedir) {
+             this.basedir = basedir;
+     }
+     
+     public void setResourceLoader(ResourceLoader resourceLoader) {
+         this.resourceLoader = resourceLoader;
+     }
+
+
+     public void afterPropertiesSet() throws Exception {
+             if (this.resourceLoader == null)
+                     throw new Exception("A resourceLoader is required.");
+             if (this.basedir == null)
+                     throw new Exception("A basedir is required.");
+             try {
+                     this.schemaDirectory = resourceLoader.getResource(basedir)
+                                     .getFile();
+             } catch (Exception e) {
+                     throw new RuntimeException(e);
+             }
+     }
+
+
+	        protected ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object command, BindException errors) throws Exception {  
+	            
+	        	try {
+	        		
+	        	FileUploadBean fileUploadBean = (FileUploadBean) command;  
+	            MultipartFile files = fileUploadBean.getFile();
+	            String tmpFileName = String.valueOf(UUID.randomUUID());
+	            this.schemaDirectory.mkdir();
+	            File mvt = new File(this.schemaDirectory,tmpFileName);
+	            files.transferTo(mvt);
+	            // System.out.println("Uploaded file:" + mvt.getAbsolutePath());
+	            
+                response.getWriter().print("#filename#" + mvt.toString() + "#/filename#");
+                response.setStatus(HttpServletResponse.SC_OK);
+
+
+	        	}
+	        	catch (Exception e) {
+	        	    log.error(e.getMessage());
+                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				}
+	        	
+                // return the result to the client
+	        	
+                response.getWriter().flush();
+
+	           return null;  
+	       }  
+	     
+	       // this method is overriding, and specify how spring convert multipart into a byte array that binds to our command class  
+	       protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {  
+	           binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());  
+	           super.initBinder(request, binder);  
+	       }  
+}  
