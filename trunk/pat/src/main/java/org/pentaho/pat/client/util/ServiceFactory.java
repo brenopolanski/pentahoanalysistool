b6c72ab9a5cd9ebe5ba0d6fa60ea23cdeb0945b0
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-03-06 10:01:59 +0000

    activate RPC interfaces and add it to the servicefactory

--- trunk/pat/src/main/java/org/pentaho/pat/client/util/ServiceFactory.java
+++ trunk/pat/src/main/java/org/pentaho/pat/client/util/ServiceFactory.java
@@ -5,6 +5,12 @@ package org.pentaho.pat.client.util;
 
 import org.pentaho.pat.client.services.Olap4JService;
 import org.pentaho.pat.client.services.Olap4JServiceAsync;
+import org.pentaho.pat.rpc.Discovery;
+import org.pentaho.pat.rpc.DiscoveryAsync;
+import org.pentaho.pat.rpc.Session;
+import org.pentaho.pat.rpc.SessionAsync;
+
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 
@@ -23,5 +29,29 @@ public class ServiceFactory {
 	      endpoint.setServiceEntryPoint(moduleRelativeURL);
 	    }
 	    return service;
+	  }
+	  
+	  static DiscoveryAsync dService = null;
+	  
+	  public static DiscoveryAsync getDiscoveryInstance() {
+	    if (dService == null) {
+	      dService = (DiscoveryAsync) GWT.create(Discovery.class);
+	      ServiceDefTarget endpoint = (ServiceDefTarget) dService;
+	      String moduleRelativeURL = GWT.getModuleBaseURL() + "discovery.rpc"; //$NON-NLS-1$
+	      endpoint.setServiceEntryPoint(moduleRelativeURL);
+	    }
+	    return dService;
+	  }
+	  
+static SessionAsync sService = null;
+	  
+	  public static SessionAsync getSessionInstance() {
+	    if (sService == null) {
+	      sService = (SessionAsync) GWT.create(Session.class);
+	      ServiceDefTarget endpoint = (ServiceDefTarget) sService;
+	      String moduleRelativeURL = GWT.getModuleBaseURL() + "session.rpc"; //$NON-NLS-1$
+	      endpoint.setServiceEntryPoint(moduleRelativeURL);
+	    }
+	    return sService;
 	  }
 }
