c21baa230cd02b5c5f229214eb8f576c8b4c2e31
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-03-06 13:17:32 +0000

    make sessionService static, so it won't be null

--- trunk/pat/src/main/java/org/pentaho/pat/server/servlet/SessionServlet.java
+++ trunk/pat/src/main/java/org/pentaho/pat/server/servlet/SessionServlet.java
@@ -13,78 +13,78 @@ public class SessionServlet extends AbstractServlet implements Session, Initiali
 
 	private static final long serialVersionUID = 1L;
 	
-	private SessionService sessionService;
+	private static SessionService sessionService;
 	
 	public void setSessionService(SessionService service) {
-		this.sessionService = service;
+		sessionService = service;
 	}
 	
 	public void afterPropertiesSet() throws Exception {
-		if (this.sessionService==null)
+		if (sessionService==null)
 			throw new Exception("A sessionService is required.");
 	}
 
 	public Boolean connect(String sessionId, String driverClassName, String url, 
 		String username, String password) throws OlapException {
-		this.sessionService.createConnection(getCurrentUserId(),sessionId, 
+		sessionService.createConnection(getCurrentUserId(),sessionId, 
 			driverClassName, url, username, password);
 		return true;
 	}
 
 	public String createNewQuery(String sessionId) throws OlapException {
-		return this.sessionService.createNewQuery(getCurrentUserId(), sessionId);
+		return sessionService.createNewQuery(getCurrentUserId(), sessionId);
 	}
 
 	public Boolean deleteQuery(String sessionId, String queryId) {
-		this.sessionService.releaseQuery(getCurrentUserId(), sessionId, queryId);
+		sessionService.releaseQuery(getCurrentUserId(), sessionId, queryId);
 		return true;
 	}
 
 	public Boolean disconnect(String sessionId) {
-		this.sessionService.releaseConnection(getCurrentUserId(),sessionId);
+		sessionService.releaseConnection(getCurrentUserId(),sessionId);
 		return true;
 	}
 
 	public String getCurrentCube(String sessionId) {
-		return (String)this.sessionService.getUserSessionVariable(
+		return (String)sessionService.getUserSessionVariable(
 				getCurrentUserId(), sessionId, Constants.CURRENT_CUBE_NAME);
 	}
 
 	public String getCurrentQuery(String sessionId) {
-		return (String)this.sessionService.getUserSessionVariable(
+		return (String)sessionService.getUserSessionVariable(
 				getCurrentUserId(), sessionId, Constants.CURRENT_QUERY_NAME);
 	}
 
 	public List<String> getQueries(String sessionId) {
-		return this.sessionService.getQueries(getCurrentUserId(), sessionId);
+		return sessionService.getQueries(getCurrentUserId(), sessionId);
 	}
 
 	public Boolean setCurrentCube(String sessionId, String cubeId) {
-		this.sessionService.saveUserSessionVariable(getCurrentUserId(), 
+		sessionService.saveUserSessionVariable(getCurrentUserId(), 
 			sessionId, Constants.CURRENT_CUBE_NAME, cubeId);
 		return true;
 	}
 
 	public Boolean setCurrentQuery(String sessionId, String queryId) {
-		this.sessionService.saveUserSessionVariable(getCurrentUserId(), 
+		sessionService.saveUserSessionVariable(getCurrentUserId(), 
 				sessionId, Constants.CURRENT_QUERY_NAME, queryId);
 		return true;
 	}
 	
 	public SelectNode getQuery(String sessionId, String queryId) {
-		return this.sessionService.getQuery(getCurrentUserId(), 
+		return sessionService.getQuery(getCurrentUserId(), 
 				sessionId, queryId);
 	}
 	
 	
 
 	public Boolean closeSession(String sessionId) {
-		this.sessionService.releaseSession(getCurrentUserId(), sessionId);
+		sessionService.releaseSession(getCurrentUserId(), sessionId);
 		return true;
 	}
 
 	public String createSession() {
-		return this.sessionService.createNewSession(getCurrentUserId());
+		return sessionService.createNewSession(getCurrentUserId());
 	}
 
 }
