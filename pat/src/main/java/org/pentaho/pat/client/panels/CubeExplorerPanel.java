4bc05506747910aec7ade43abe1baf03c4cc6353
tom@wamonline.org.uk <tom@wamonline.org.uk@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-03-10 07:33:51 +0000

    Broken for the next 90 minutes, tree graphics

--- pat/src/main/java/org/pentaho/pat/client/panels/CubeExplorerPanel.java
+++ pat/src/main/java/org/pentaho/pat/client/panels/CubeExplorerPanel.java
@@ -6,8 +6,12 @@ package org.pentaho.pat.client.panels;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+
+import org.gwt.mosaic.showcase.client.ContentWidget;
 import org.gwt.mosaic.ui.client.CaptionLayoutPanel;
 import org.gwt.mosaic.ui.client.ListBox;
 import org.gwt.mosaic.ui.client.layout.BoxLayout;
@@ -24,12 +28,17 @@ import org.pentaho.pat.client.util.GuidFactory;
 import org.pentaho.pat.client.util.ServiceFactory;
 import org.pentaho.pat.client.widgets.DemoFlexTable;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeImages;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.client.FastTree;
 import com.google.gwt.widgetideas.client.FastTreeItem;
@@ -42,7 +51,9 @@ import com.google.gwt.widgetideas.client.HasFastTreeItems;
  */
 public class CubeExplorerPanel extends CaptionLayoutPanel implements ConnectionListener,
 SourcesConnectionEvents  {
+	
 
+	public static final CubeImages IMAGES = (CubeImages) GWT.create(CubeImages.class);
 	private static final String AXIS_NONE = "none"; //$NON-NLS-1$
 	private static final String AXIS_UNUSED = "UNUSED"; //$NON-NLS-1$
 	private static final String AXIS_FILTER = "FILTER"; //$NON-NLS-1$
@@ -55,12 +66,34 @@ SourcesConnectionEvents  {
 	private static FastTreeItem a;
 	private ConnectionListenerCollection connectionListeners;
 	public static FlexTableCellDragController tableRowDragController;
-
+	private static TreeItem rootNode;
 	public CubeExplorerPanel(String text) {
 		super(text);
 		
 	    init();
 	}
+	  /**
+	   * Images used in the {@link Application}.
+	   */
+	  public interface CubeTreeImages extends TreeImages {
+	    /**
+	     * An image indicating a leaf.
+	     * 
+	     * @return a prototype of this image
+	     */
+	    @Resource("noimage.png")
+	    AbstractImagePrototype treeLeaf();
+	  }
+	  
+	  /**
+	   * A mapping of history tokens to their associated menu items.
+	   */
+	  private Map<String, TreeItem> itemTokens = new HashMap<String, TreeItem>();
+
+	  /**
+	   * A mapping of menu items to the widget display when the item is selected.
+	   */
+	  private Map<TreeItem, ContentWidget> itemWidgets = new HashMap<TreeItem, ContentWidget>();
 
 	public void init() {
 		
@@ -70,10 +103,13 @@ SourcesConnectionEvents  {
 			    vBox.setPadding(0);
 			    vBox.setWidgetSpacing(0);
 
-	    
-			    final FastTree t = new FastTree();
-			    a = t.addItem("Available Cubes");
-
+			    CubeTreeImages treeImages = GWT.create(CubeTreeImages.class);
+			    final Tree t = new Tree(treeImages);
+			    t.setAnimationEnabled(true);
+			    t.addStyleName("cube-menu");
+			    
+			    rootNode = t.addItem("Available Cubes");
+			    
 		
 			    final ScrollPanel panel = new ScrollPanel();
 			    vBox.add(panel);
@@ -97,6 +133,7 @@ SourcesConnectionEvents  {
 								a.removeItems();
 							
 								a.addItem(cubeNames[i][1]);
+								
 							}
 						}
 					}
@@ -115,7 +152,23 @@ SourcesConnectionEvents  {
 		axis.add(AXIS_FILTER);
 		populateDimensions(axis);
 	}
+	  /**
+	   * Add an option to the main menu.
+	   * 
+	   * @param parent the {@link TreeItem} that is the option
+	   * @param content the {@link ContentWidget} to display when selected
+	   * @param image the icon to display next to the {@link TreeItem}
+	   */
+	  private static void setupMainMenuOption(TreeItem parent, String name,
+	      AbstractImagePrototype image) {
+	    // Create the TreeItem
+	    TreeItem option = parent.addItem(image.getHTML() + " " + name);
 
+	    // Map the item to its history token and content widget
+	    itemWidgets.put(option, content);
+	    itemTokens.put(getContentWidgetToken(content), option);
+	  }
+	  
 	public static void populateDimensions(List axis) {
 		if (axis.contains(AXIS_NONE)) {
 			ServiceFactory.getInstance().getDimensions(AXIS_NONE,
@@ -126,8 +179,9 @@ SourcesConnectionEvents  {
 					a.removeItems();
 
 					for (int j = 0; j < dimStrs.length; j++) {
-						a.addItem(dimStrs[j]);
-						
+						//a.addItem(dimStrs[j]);
+						setupMainMenuOption(rootNode, dimStrs[j],
+						        IMAGES.calendar());		
 					}
 
 				}
---
+++ pat/src/main/java/org/pentaho/pat/client/panels/CubeImages.java
@@ -0,0 +1,10 @@
+package org.pentaho.pat.client.panels;
+
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.ImageBundle;
+
+public interface CubeImages extends ImageBundle{
+
+	 AbstractImagePrototype calendar();
+	 
+}
