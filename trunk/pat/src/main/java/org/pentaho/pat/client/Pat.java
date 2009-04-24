37cfc63438702a0a9e786663b086e90c918d8502
tom@wamonline.org.uk <tom@wamonline.org.uk@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-04-24 20:34:31 +0000

    Licences and stuff

--- trunk/pat/src/main/java/org/pentaho/pat/client/Pat.java
+++ trunk/pat/src/main/java/org/pentaho/pat/client/Pat.java
@@ -1,17 +1,13 @@
 /*
- * Copyright 2009 Thomas Barber.  All rights reserved. 
- * This software was developed by Thomas Barber and is provided under the terms 
- * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
- * this file except in compliance with the license. If you need a copy of the license, 
- * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
- * Analysis Tool.  The Initial Developer is Thomas Barber.
+ * Copyright (C) 2009 Tom Barber
  *
- * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
- * the license for the specific language governing your rights and limitations.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * @created Apr 23, 2009
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA *
+ *
+ * @created Apr 23, 2009 
  * @author Tom Barber
  */
 
