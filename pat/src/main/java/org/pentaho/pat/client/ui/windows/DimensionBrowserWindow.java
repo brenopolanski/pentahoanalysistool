3085824e6e0668d963ceb237f1835d245757a67f
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2009-08-18 17:48:30 +0000

    first implementation of dimension browser (for selections)

---
+++ pat/src/main/java/org/pentaho/pat/client/ui/windows/DimensionBrowserWindow.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2009 Paul Stoellberger
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ *
+ */
+package org.pentaho.pat.client.ui.windows;
+
+
+import org.gwt.mosaic.ui.client.WindowPanel;
+import org.gwt.mosaic.ui.client.layout.BoxLayout;
+import org.gwt.mosaic.ui.client.layout.BoxLayoutData;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
+import org.gwt.mosaic.ui.client.layout.BoxLayout.Orientation;
+import org.gwt.mosaic.ui.client.layout.BoxLayoutData.FillStyle;
+import org.pentaho.pat.client.ui.panels.DimensionMenu;
+
+/**
+ * Lists all Dimensions and Members and allows their including/excluding in a Query.
+ * @created Aug 18, 2009 
+ * @since 0.5.0
+ * @author Paul Stoellberger
+ * 
+ */
+public class DimensionBrowserWindow extends WindowPanel {
+
+    /** The Window Title. */
+    private static final String TITLE = "Dimension Browser";
+
+    private static final DimensionMenu dimensionMenuPanel = new DimensionMenu();
+
+    private static final LayoutPanel windowContentpanel = new LayoutPanel(new BoxLayout(Orientation.HORIZONTAL));
+    
+    private static final DimensionBrowserWindow dbw = new DimensionBrowserWindow();
+    
+    private static String query;
+    
+    private static String dimension;
+    /**
+     * Cube Browser Window Constructor.
+     */
+    public DimensionBrowserWindow() {
+        super(TITLE);
+        windowContentpanel.add(dimensionMenuPanel, new BoxLayoutData(FillStyle.BOTH));
+        this.setWidget(windowContentpanel);
+        this.layout();
+        
+    }
+    
+    // public static void displayAllDimensions(final String queryId) {
+    //    query = queryId;
+    //    display(true);
+    //}
+
+    public static void displayDimension(final String queryId, final String dimension) {
+        query = queryId;
+        DimensionBrowserWindow.dimension = dimension;
+        dimensionMenuPanel.loadMembers(queryId, dimension);
+        display();
+    }
+    
+    private static void display() {
+        dbw.setSize("450px", "300px");
+        dbw.showModal(false);
+        dbw.layout();
+    }
+    
+
+}
+
