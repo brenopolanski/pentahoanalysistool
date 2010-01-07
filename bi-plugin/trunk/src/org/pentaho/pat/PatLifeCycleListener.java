effed6879e98573f0bec800ec55cbbd64f6c045a
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-01-07 00:11:59 +0000

    hibernate integration - doesn't work / doesn't break anything .... giving up for now

--- bi-plugin/trunk/src/org/pentaho/pat/PatLifeCycleListener.java
+++ bi-plugin/trunk/src/org/pentaho/pat/PatLifeCycleListener.java
@@ -1,9 +1,16 @@
 package org.pentaho.pat;
 
+import java.io.File;
 import java.net.URL;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
 import org.pentaho.pat.server.servlet.DiscoveryServlet;
 import org.pentaho.pat.server.servlet.QueryServlet;
 import org.pentaho.pat.server.servlet.SessionServlet;
@@ -14,7 +21,9 @@ import org.pentaho.platform.api.engine.PluginLifecycleException;
 import org.pentaho.platform.api.engine.ServiceException;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 
 public class PatLifeCycleListener implements IPluginLifecycleListener {
 
@@ -45,11 +54,45 @@ public class PatLifeCycleListener implements IPluginLifecycleListener {
                 String appContextUrl = contextUrl.toString();
 
                 ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(new String[] { appContextUrl }, false);
-                System.out.println("########## CLASSLOADER:" + PatLifeCycleListener.class.getClassLoader().toString());
 
                 applicationContext.setClassLoader(pluginClassloader);
                 applicationContext.setConfigLocation(appContextUrl);
                 applicationContext.refresh();
+
+                String hibernateConfigurationFile =  PentahoSystem.getSystemSetting( "hibernate/hibernate-settings.xml","settings/config-file", null);
+                String configPath = PentahoSystem.getApplicationContext().getSolutionPath(hibernateConfigurationFile);
+                Configuration ac = new Configuration();
+                ac.configure(new File(configPath));
+
+                applicationContext.setAllowBeanDefinitionOverriding(true);
+                BasicDataSource a = (BasicDataSource)applicationContext.getBean("dataSource");
+                a.setDriverClassName(ac.getProperties().getProperty("connection.driver_class"));
+                a.setUrl(ac.getProperties().getProperty("connection.url"));
+                a.setUsername(ac.getProperties().getProperty("connection.username"));
+                a.setPassword(ac.getProperties().getProperty("connection.password"));
+
+
+                URL hibernateUrl = pluginClassloader.getResource("pat-hibernate.cfg.xml");
+                AnnotationConfiguration patConfig = new AnnotationConfiguration();
+                patConfig.configure(hibernateUrl);
+                //                patConfig.setProperties(ac.getProperties());
+
+
+                SessionFactory sf = (SessionFactory)applicationContext.getBean("sessionFactory");
+                //                sf = patConfig.buildSessionFactory();
+
+                LocalSessionFactoryBean sb = (LocalSessionFactoryBean) applicationContext.getBean("&sessionFactory");
+                //                sb.setDataSource(a);
+                sb.setBeanClassLoader(pluginClassloader);
+                Properties aproperties = new Properties();
+                aproperties = patConfig.getProperties();
+                aproperties.remove("connection.url");
+                aproperties.put("connection.url", ac.getProperty("connection.url"));
+
+                sb.setHibernateProperties(aproperties);
+                sb.afterPropertiesSet();
+
+                sf = (SessionFactory) sb.getObject();
 
 
                 ((SessionServlet)targetSessionBean).setStandalone(true);
@@ -64,7 +107,7 @@ public class PatLifeCycleListener implements IPluginLifecycleListener {
                 ((DiscoveryServlet)targetDiscoveryBean).setStandalone(true);
                 DiscoveryServlet.setApplicationContext(applicationContext);
                 ((DiscoveryServlet)targetDiscoveryBean).init();
-                
+
             }
             else
             {
@@ -74,6 +117,9 @@ public class PatLifeCycleListener implements IPluginLifecycleListener {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         } catch (ServletException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
