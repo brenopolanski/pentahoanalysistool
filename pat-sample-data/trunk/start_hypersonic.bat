9208972d17a998b9cd303718d9f6914a5527348a
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-03-04 21:25:29 +0000

    add Xmx to hypersonic start scripts

--- pat-sample-data/trunk/start_hypersonic.bat
+++ pat-sample-data/trunk/start_hypersonic.bat
@@ -26,12 +26,12 @@ call "%~dp0set-java.bat"
 REM -----------------------
 REM SAMPLEDATA + HIBERNATE
 REM -----------------------
-"%_JAVA%" -cp %tempclasspath% org.hsqldb.Server -database.0 hsqldb\sampledata -dbname.0 sampledata -database.1 hsqldb\hibernate -dbname.1 hibernate
+"%_JAVA%" -Xmx256m -cp %tempclasspath% org.hsqldb.Server -database.0 hsqldb\sampledata -dbname.0 sampledata -database.1 hsqldb\hibernate -dbname.1 hibernate
 
 REM -----------------------
 REM  WITH FOODMART
 REM -----------------------
-REM "%_JAVA%" -cp %tempclasspath% org.hsqldb.Server -database.0 hsqldb\sampledata -dbname.0 sampledata -database.1 hsqldb\hibernate -dbname.1 hibernate -database.1 hsqldb\foodmart -dbname.1 foodmart
+REM "%_JAVA%" -Xmx256m -cp %tempclasspath% org.hsqldb.Server -database.0 hsqldb\sampledata -dbname.0 sampledata -database.1 hsqldb\hibernate -dbname.1 hibernate -database.1 hsqldb\foodmart -dbname.1 foodmart
 
 echo %command%
 %command%
