c998acd260ef875509a3878afaf2600d5df78847
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-05-31 15:50:44 +0000

    more work on query serializing etc.

---
+++ trunk/pat/src/main/java/org/pentaho/pat/server/util/serializer/QueryParseException.java
@@ -0,0 +1,18 @@
+package org.pentaho.pat.server.util.serializer;
+
+public class QueryParseException extends Exception {
+
+    public QueryParseException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+
+    public QueryParseException(String arg0) {
+        super(arg0);
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1674892615129992198L;
+
+}
