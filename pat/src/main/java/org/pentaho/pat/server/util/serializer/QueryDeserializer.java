8fb04d1a2c107e09f6b76d65d6b65fc15d1e93a4
paul.stoellberger@aschauer-edv.at <paul.stoellberger@aschauer-edv.at@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-06-06 23:29:55 +0000

    switch query deserializer to JDOM

--- pat/src/main/java/org/pentaho/pat/server/util/serializer/QueryDeserializer.java
+++ pat/src/main/java/org/pentaho/pat/server/util/serializer/QueryDeserializer.java
@@ -5,15 +5,10 @@ import java.io.StringReader;
 import java.sql.SQLException;
 import java.util.Iterator;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.apache.commons.lang.StringUtils;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 import org.olap4j.Axis;
 import org.olap4j.OlapConnection;
 import org.olap4j.OlapException;
@@ -27,11 +22,9 @@ import org.olap4j.query.QueryDimension;
 import org.olap4j.query.Selection;
 import org.olap4j.query.SortOrder;
 import org.olap4j.query.QueryDimension.HierarchizeMode;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 import org.xml.sax.InputSource;
+
+
 
 
 public class QueryDeserializer {
@@ -42,34 +35,33 @@ public class QueryDeserializer {
     private static String xml;
     private static OlapConnection connection;
     private static InputSource source;
-    final private static XPath xpath = XPathFactory.newInstance().newXPath();
-    
+
     public static PatQuery unparse(String xml, OlapConnection connection) throws Exception {
-        
+
         QueryDeserializer.connection = connection;
         QueryDeserializer.xml = xml;
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db;
-        try {
-            db = dbf.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            throw new Exception("error creating document config");
-        }
-        
+
+        SAXBuilder parser = new SAXBuilder();
+
+        //get the dom-document
+
         source = new InputSource((new ByteArrayInputStream(xml.getBytes())));
-        
-
-        dom = db.parse((new InputSource(new StringReader(xml))));
-
-        NodeList qm = dom.getElementsByTagName("QueryModel");
+        dom = parser.build(source);
+        Element child =(Element) dom.getRootElement();
+        System.out.println("Root:" + child.getName());
+        System.out.println("Children:" + child.getChildren().size());
+        Element qmElement = child.getChild("QueryModel");
         PatQuery returnQuery = null;
-        if (qm != null) {
+        if (qmElement != null) {
             returnQuery = createQmQuery();
             return returnQuery;
-        } else if (dom.getElementsByTagName("MDX") != null) {
-            NodeList mdxq = dom.getElementsByTagName("MDX");
-            returnQuery = createMdxQuery();
         }
+        else
+            System.out.println("No QueryModel");
+
+        //        if (child.getChild("MDX") != null) {
+        //            returnQuery = createMdxQuery();
+        //        }
 
 
 
@@ -84,40 +76,28 @@ public class QueryDeserializer {
 
     private static PatQuery createQmQuery() throws QueryParseException {
 
-        NodeList queryElement = dom.getElementsByTagName("Query");
-        if (queryElement != null) {
-            Node qnode = queryElement.item(0);
+        Element queryElement = dom.getRootElement();
+        if (queryElement != null && queryElement.getName().equals("Query")) {
 
-            Node nameAttr = qnode.getAttributes().getNamedItem("name");
-            String queryName = null;
-            if (nameAttr != null && StringUtils.isNotBlank(nameAttr.getNodeValue())) {
-                queryName = nameAttr.getNodeValue();
-            }
-
-
-            Node qnodeAttr = qnode.getAttributes().getNamedItem("cube");
-            String cubeName = null;
-            if (qnodeAttr != null && StringUtils.isNotBlank(qnodeAttr.getNodeValue())) {
-                cubeName = qnodeAttr.getNodeValue();
-            }
-
+            String queryName = queryElement.getAttributeValue("name");
+            String cubeName = queryElement.getAttributeValue("cube");
 
             if (!StringUtils.isNotBlank(cubeName)) {
                 throw new QueryParseException("Cube for query not defined");
             }
 
-            Node catAttr = qnode.getAttributes().getNamedItem("catalog");
-            String catalogName = null;
-            if (catAttr != null && StringUtils.isNotBlank(catAttr.getNodeValue())) {
-                catalogName = qnodeAttr.getNodeValue();
-            }
+            String catalogName = queryElement.getAttributeValue("catalog");
 
             try {
-                qm = createEmptyQuery(queryName,catalogName, cubeName);
+                Element qmElement = queryElement.getChild("QueryModel");
+                if (qmElement != null) {
+                    qm = createEmptyQuery(queryName,catalogName, cubeName);
+                    manipulateQuery(qmElement);
+                    return new QmQuery(connection,qm);
+                }
+                else
+                    throw new OlapException("Can't find child <QueryModel>");
 
-                manipulateQuery();
-                return new QmQuery(connection,qm);
-                
             } catch (OlapException e) {
                 throw new QueryParseException(e.getMessage(),e);
             }
@@ -131,124 +111,112 @@ public class QueryDeserializer {
 
     }
 
-    private static void manipulateQuery() throws OlapException {
-        moveDims2Axis();
-        
-        
+    private static void manipulateQuery(Element qmElement) throws OlapException {
+        moveDims2Axis(qmElement);
+
+
     }
 
-    private static void moveDims2Axis() throws OlapException {
-        NodeList axis = dom.getElementsByTagName("Axis");
-        for (int i = 0; i < axis.getLength();i++) {
-            if (axis.item(i).getNodeType() == Node.ELEMENT_NODE) {
-            String location = null;
-            Node attrNode = axis.item(i).getAttributes().getNamedItem("location");
-            if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                location = attrNode.getNodeValue();
-            }
-            else 
-                throw new OlapException("Location for Axis Element can't be null");
-            
-            QueryAxis qAxis = qm.getAxes().get(getAxisName(location));
-            
-            String nonEmpty = null;
-            attrNode = axis.item(i).getAttributes().getNamedItem("nonEmpty");
-            if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                nonEmpty = attrNode.getNodeValue();
-                qAxis.setNonEmpty(Boolean.getBoolean(nonEmpty));
-            }
-            
-            String sortOrder = null;
-            attrNode = axis.item(i).getAttributes().getNamedItem("sortOrder");
-            if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                sortOrder = attrNode.getNodeValue();
-            }
-            
-            String sortEvaluationLiteral = null;
-            attrNode = axis.item(i).getAttributes().getNamedItem("sortEvaluationLiteral");
-            if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                sortEvaluationLiteral = attrNode.getNodeValue();
-            }
+    private static void moveDims2Axis(Element qmElement) throws OlapException {
+        Element axesElement = qmElement.getChild("Axes");
+        if (axesElement != null) {
 
-            if (StringUtils.isNotBlank(sortOrder)) {
-                if (StringUtils.isNotBlank(sortEvaluationLiteral)) {
-                    qAxis.sort(SortOrder.valueOf(sortOrder),sortEvaluationLiteral);
-                }
-                else {
-                    qAxis.sort(SortOrder.valueOf(sortOrder));
-                }
-            }
-            
-            String searchPath = "/Query/QueryModel/Axes/Axis[@location=\"" + location + "\"]//Dimension";
-            System.out.println(searchPath);
-            NodeList dimensions = null;
-            try {
-                dimensions = (NodeList) xpath.evaluate(searchPath, dom, XPathConstants.NODESET);
-                processDimension(dimensions, location);
-            } catch (XPathExpressionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            for(int i = 0; i < axesElement.getChildren("Axis").size(); i++) {
+                Element axisElement = (Element) axesElement.getChildren("Axis").get(i);
 
-           
-            }
-        }
-        
-    }
-
-    private static void processDimension(NodeList dimensions, String location) throws OlapException {
-
-        for (int z = 0; z < dimensions.getLength(); z++) {
-             Node attrNode = dimensions.item(z).getAttributes().getNamedItem("name");
-            if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                String dimName = attrNode.getNodeValue();
-                dimName = dimName.substring(1,dimName.length() - 1);
-                System.out.println("dimName:"  + dimName);
-                QueryDimension dim = qm.getDimension(dimName);
-                
-                if (dim == null)
-                    throw new OlapException("Dimension not found:" + dimName);
-                
-                String sortOrder = null;
-                attrNode = dimensions.item(z).getAttributes().getNamedItem("sortOrder");
-                if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                    sortOrder = attrNode.getNodeValue();
-                    dim.sort(SortOrder.valueOf(sortOrder));
+                String location = axisElement.getAttributeValue("location");
+                if (!StringUtils.isNotBlank(location)) {
+                    throw new OlapException("Location for Axis Element can't be null");
                 }
-                
-                String hierarchizeMode = null;
-                attrNode = dimensions.item(z).getAttributes().getNamedItem("hierarchizeMode");
-                if (attrNode != null && StringUtils.isNotBlank(attrNode.getNodeValue())) {
-                    hierarchizeMode = attrNode.getNodeValue();
-                    dim.setHierarchizeMode(HierarchizeMode.valueOf(hierarchizeMode));
+
+                QueryAxis qAxis = qm.getAxes().get(getAxisName(location));
+
+                String nonEmpty = axisElement.getAttributeValue("nonEmpty");
+                if (StringUtils.isNotBlank(nonEmpty)) {
+                    qAxis.setNonEmpty(Boolean.parseBoolean(nonEmpty));
                 }
-                
-                qm.getAxes().get(Axis.Standard.valueOf(location)).getDimensions().add(dim);
-                
-                String searchPath = "//Dimension[@name = \"[" + dimName + "]\"]/Inclusions";
-                System.out.println(searchPath);
-                try {
-                    NodeList dimChildren =  (NodeList) xpath.evaluate(searchPath, dimensions.item(z), XPathConstants.NODESET);
-                    for (int k = 0; k < dimChildren.getLength();k++) {
-                        String name = ((Node) xpath.evaluate("//@member", dimChildren.item(k), XPathConstants.NODE)).getNodeValue();
-//                        name = name.substring(1,name.length() - 1);
-                        String operator = ((Node) xpath.evaluate("//@operator", dimChildren.item(k), XPathConstants.NODE)).getNodeValue();
-                        String memberDim = ((Node) xpath.evaluate("//@dimension", dimChildren.item(k), XPathConstants.NODE)).getNodeValue();
-                        dim.include(Selection.Operator.valueOf(operator), memberDim, name);
+
+                String sortOrder = axisElement.getAttributeValue("sortOrder");
+                String sortEvaluationLiteral = axisElement.getAttributeValue("sortEvaluationLiteral");
+
+                if (StringUtils.isNotBlank(sortOrder)) {
+                    if (StringUtils.isNotBlank(sortEvaluationLiteral)) {
+                        qAxis.sort(SortOrder.valueOf(sortOrder),sortEvaluationLiteral);
                     }
-                } catch (XPathExpressionException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    else {
+                        qAxis.sort(SortOrder.valueOf(sortOrder));
+                    }
                 }
-//                for (int h = 0; h < dimChildren.getLength(); h++) {
-//                    if ()
-//                    processInclusions(dim,)
-//                }
+
+                Element dimensions = axisElement.getChild("Dimensions");
+                if (dimensions != null) {
+                    for(int z = 0; z < dimensions.getChildren("Dimension").size(); z++) {
+                        Element dimensionElement = (Element) dimensions.getChildren("Dimension").get(z);
+                        processDimension(dimensionElement, location);
+
+                    }
+                }
             }
-            else 
-                throw new OlapException("No Dimension name defined");
-    
         }
+
+    }
+
+    private static void processDimension(Element dimension, String location) throws OlapException {
+
+        String dimName = dimension.getAttributeValue("name");
+        if (StringUtils.isNotBlank(dimName)) {
+
+            dimName = dimName.substring(1,dimName.length() - 1);
+            System.out.println("dimName:"  + dimName);
+            QueryDimension dim = qm.getDimension(dimName);
+
+            if (dim == null)
+                throw new OlapException("Dimension not found:" + dimName);
+
+            String sortOrder = dimension.getAttributeValue("sortOrder");
+            if (StringUtils.isNotBlank(sortOrder)) {
+                dim.sort(SortOrder.valueOf(sortOrder));
+            }
+
+            String hierarchizeMode = dimension.getAttributeValue("hierarchizeMode");
+            if (StringUtils.isNotBlank(hierarchizeMode)) {
+                dim.setHierarchizeMode(HierarchizeMode.valueOf(hierarchizeMode));
+            }
+
+            qm.getAxes().get(Axis.Standard.valueOf(location)).getDimensions().add(dim);
+            
+            Element inclusions = dimension.getChild("Inclusions");
+            if (inclusions != null) {
+                for(int z = 0; z < inclusions.getChildren("Selection").size(); z++) {
+                    Element selectionElement = (Element) inclusions.getChildren("Selection").get(z);
+                    String name = selectionElement.getAttributeValue("member");
+                    String operator = selectionElement.getAttributeValue("operator");
+                    String memberDim = selectionElement.getAttributeValue("dimension");
+                    dim.include(Selection.Operator.valueOf(operator), QueryDimension.getNameParts(name));
+                    
+                    // TODO ADD CONTEXT
+
+                }
+            }
+            
+            Element exclusions = dimension.getChild("Exclusions");
+            if (inclusions != null) {
+                for(int z = 0; z < exclusions.getChildren("Selection").size(); z++) {
+                    Element selectionElement = (Element) exclusions.getChildren("Selection").get(z);
+                    String name = selectionElement.getAttributeValue("member");
+                    String operator = selectionElement.getAttributeValue("operator");
+                    String memberDim = selectionElement.getAttributeValue("dimension");
+                    dim.exclude(Selection.Operator.valueOf(operator), QueryDimension.getNameParts(name));
+
+                    // TODO ADD CONTEXT
+                }
+            }
+
+        }
+        else 
+            throw new OlapException("No Dimension name defined");
+
+
     }
 
     private static Query createEmptyQuery(String queryName, String catalogName, String cubeName) throws OlapException {
@@ -293,8 +261,8 @@ public class QueryDeserializer {
             return org.olap4j.Axis.Standard.valueOf(location);
         }
         return null;
-        
+
     }
-    
+
 
 }
