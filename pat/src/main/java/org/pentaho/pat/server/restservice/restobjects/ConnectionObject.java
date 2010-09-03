febb7fa050f4d9bbf02eff98a4e65b7dfa04307d
tom@wamonline.org.uk <tom@wamonline.org.uk@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-09-03 06:46:47 +0000

    some javadoc and adding nested cubes to connections in the global object

--- pat/src/main/java/org/pentaho/pat/server/restservice/restobjects/ConnectionObject.java
+++ pat/src/main/java/org/pentaho/pat/server/restservice/restobjects/ConnectionObject.java
@@ -1,11 +1,14 @@
 package org.pentaho.pat.server.restservice.restobjects;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import org.pentaho.pat.rpc.dto.CubeItem;
 
 @XmlRootElement
 public class ConnectionObject {
@@ -17,6 +20,10 @@ public class ConnectionObject {
 		
 		@XmlElement(name = "connectionId", required = true)
 		String id;
+		
+		@XmlElement(name = "cubes", required = false)
+		List<CubeItem> cubes;
+		
 	    public void setText(String text) {
 	        this.name = text;
 	    }
@@ -24,6 +31,11 @@ public class ConnectionObject {
 	    public void setId(String text) {
 	        this.id = text;
 	    }
+
+        public void setCubes(CubeItem[] out) {
+        this.cubes = Arrays.asList(out);    
+            
+        }
 	    
 
 		
@@ -39,6 +51,17 @@ public class ConnectionObject {
 			sub.setText(name);
 			names.add(sub);
 		}
+
+
+        public void addConnection(String id, String name, CubeItem[] out) {
+            SubCob sub = new SubCob();
+            sub.setId(id);
+            sub.setText(name);
+            sub.setCubes(out);
+            names.add(sub);
+            
+            
+        }
 
 
 
