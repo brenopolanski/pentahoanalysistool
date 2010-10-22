e7d242639abc38b2dd745063962aab8ace854a1e
mark@tiemonster.info <mark@tiemonster.info@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-10-22 19:03:48 +0000

    Expanded urlconf. -MSC

--- trunk/pat-ui/rest/index.php
+++ trunk/pat-ui/rest/index.php
@@ -11,17 +11,18 @@
     define('BASE_URL', "/patui/rest");
 
     // Define routes here - See http://gluephp.com/documentation.html
+    // Trailing slashes limits the urls that hit
     $urls = array(
-    	BASE_URL . '/' => 'Rest',
-        BASE_URL . '/(?P<route1>[-\w]+)/' => 'Rest',
-        BASE_URL . '/(?P<route1>[-\w]+)/(?P<route2>[-\w]+)/' => 'Rest',
-        BASE_URL . '/(?P<route1>[-\w]+)/(?P<route2>[-\w]+)/(?P<route3>[-\w]+)/' => 'Rest'
+    	BASE_URL . '' => 'Rest',
+        BASE_URL . '/(?P<route1>[-\w]+)' => 'Rest',
+        BASE_URL . '/(?P<route1>[-\w]+)/(?P<route2>[-\w]+)' => 'Rest',
+        BASE_URL . '/(?P<route1>[-\w]+)/(?P<route2>[-\w]+)/(?P<route3>[-\w]+)' => 'Rest'
     );
 	
     try {
     	glue::stick($urls);
     } catch (BadMethodCallException $e) {
-    	header("HTTP/1.0 404 Not Found");
+    	header("HTTP/1.0 500 Internal Server Error");
     	$output = array( 'error'=>'There was an error with your request.' );
 		die(json_encode($output)); // I've always loved the sweet justice of this function
     } catch (Exception $e) {
