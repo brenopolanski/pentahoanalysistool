fb52edd316fea12a5c8ca6f0198db2a6e86c6216
prashant.g.raju@gmail.com <prashant.g.raju@gmail.com@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-10-23 13:21:51 +0000

    Updated HTML/CSS/JS. Now includes drop areas, table style, buttons.

--- pat-ui/js/patui/view.js
+++ pat-ui/js/patui/view.js
@@ -3,42 +3,55 @@
  */
 
 var view = {
-	/*
+    /*
 	 * This is where you're going to put your initial UI drawing
 	 */
-	drawUI: function() {
+    drawUI: function() {
 	
-		// Ensure that document has lazy loaded whole view file
-		$(document).ready(function() {
+        // Ensure that document has lazy loaded whole view file
+        $(document).ready(function() {
 			
-		    //  Initialise BlockUI for loading available schemas and cubes
-		    $.blockUI({
-		        message: '<div class="blockOverlay-inner">'+PAT_TITLE+'<br/>Loading <span id="blockOverlay-update">schemas and cubes...</span></div>'
-		    });
+            //  Initialise BlockUI for loading available schemas and cubes
+            // $.blockUI({
+            //     message: '<div class="blockOverlay-inner">'+PAT_TITLE+'<br/>Loading <span id="blockOverlay-update">schemas and cubes...</span></div>'
+            // });
 		    
-		    //  Intialise jQuery UI Layout
-		    $('body').layout({
-		        north__closable:          false,
-		        north__resizable:         false,
-		        north__spacing_open:      4,
-		        north__spacing_closed:    4,
-		        north__size:              30,
-		        west__spacing_open:       4,
-		        west__spacing_closed:     4,
-		        west__size:               250,
-		        west__resizable:          true
-		    });
-		});
+            // Intialise jQuery UI Layout
+            $('body').layout({
+                fxName:                 "slide",
+                fxSpeed:                0.00001,
+                north__closable:        false,
+                north__resizable:       false,
+                north__spacing_open:    0,
+                north__spacing_closed:  0,
+                west__spacing_open:     4,
+                west__spacing_closed:   4,
+                west__resizable:        true,
+                west__closable:         true,
+                west__size:             250
+            });
 
-                // New query method controller , newQueryButtonClick
-	},
+            // jQuery UI Tabs
+            var $tabs = $('#querylist_container').tabs();
+
+            // If close button is clicked
+            $('#querylist_container li a img').click(function() {
+                alert('Are you sure you want to do that?');
+                var index = $('li',$tabs).index($(this).parent());
+                $tabs.tabs('remove', index);
+            });
+            
+        });
+
+    // New query method controller , newQueryButtonClick
+    },
 	
-	processing: function(message) {
-		// Block the UI
-		// TODO - handle the case that the UI is already blocked
-	}, 
+    processing: function(message) {
+    // Block the UI
+    // TODO - handle the case that the UI is already blocked
+    },
 	
-	free: function() {
-		// Unblock UI
-	}
+    free: function() {
+    // Unblock UI
+    }
 };
