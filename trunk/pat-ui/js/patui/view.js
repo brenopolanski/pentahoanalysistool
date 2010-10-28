05fab85eece7623e7f731694f3c45ba53355d2b8
mark@tiemonster.info <mark@tiemonster.info@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-10-28 18:42:45 +0000

    [BROKEN] Took a shot at the tab architecture. Navigation is broken. Design is jacked up for tabs. Selected tab not giving feedback in the tab bar. Clicking a tab changes the current url. Data is not being stored correctly in cube navigation.

--- trunk/pat-ui/js/patui/view.js
+++ trunk/pat-ui/js/patui/view.js
@@ -3,6 +3,16 @@
  */
 
 var view = {
+	/*
+	 * The navigation that will be used to select a cube
+	 */
+	navigation: "",
+	
+	/*
+	 * The number of tabs that have been rendered, for naming new tabs
+	 */
+	rendered_tabs: 0,
+	
     /*
      * This is where you're going to put your initial UI drawing
      */
@@ -52,19 +62,6 @@ var view = {
                 center__onresize:       resize_tab_panel_layout
             });
 
-            window.tabs_loading = true;
-
-            $tabs = $("#tab_list");
-            $tabs.tabs({
-                show: function (evt, ui) {
-                    if (tabs_loading) {
-                        tabs_loading = false;
-                        outer_layout.resizeAll();
-                    }
-                    resize_tab_panel_layout(ui);
-                }
-            });
-
             // Add event handler to toolbar buttons
             $("#toolbar a").click(function() {
                 controller.click_handler($(this));
@@ -74,11 +71,25 @@ var view = {
         });
     },
     
+    /*
+     * Clear credentials and clear the screen
+     */
     logout: function() {
+    	// Delete the cached navigation
+    	view.navigation = "";
+    	
+    	// Remove all tabs
+    	$('#tab_list ul li').remove();
+    	$('#tab_content div').remove();
+    	
+    	// Hide the layout
         $("#header").hide();
         $("#tab_content").hide();
     },
 	
+    /*
+     * Block the UI and display a status message
+     */
     processing: function(message) {
         // Block the UI
         $.blockUI({
@@ -93,46 +104,101 @@ var view = {
         });
     },
 	
+    /*
+     * Free the UI for interaction again
+     */
     free: function() {
         // Unblock UI
         $.unblockUI();
     },
     
-    generate_navigation: function($connections) {
-        view.processing("Loading schema...");
+    /*
+     * Load data list with schema and cubes from the PAT server
+     */
+    generate_navigation: function($tab) {
+        $data_list = $tab.find('.data_list');
     	
-        // Iterate over connections and populate navigation
-        $.each(model.connections.connections.connection, function(i,connection){
-            $.each(connection.schemas, function(i,schema){
-                $('.data_list').append('<optgroup label="'+schema['@schemaname']+'">');
-                $.each(schema.cubes, function(i,cube){
-                    if(cube.length == undefined)
-                        cube = [cube];
-                    $.each(cube, function(i,item){
-                        $("<option />")
-                        .attr({
-                            'value': connection['@connectionid']
-                        })
-                        .data({
-                            'schema': schema['@schemaname'],
-                            'cube': item['@cubename']
-                        })
-                        .text(item['@cubename'])
-                        .appendTo($('.data_list'));
-                    });
-                });
-                $('.data_list').append('</optgroup>');
-            });
+        // Check to see if navigation is cached
+        if (view.navigation == "") {
+        	// Block the UI - depending on connection, this may take a while
+        	view.processing("Loading schema...");
+        	
+        	// Iterate over connections and populate navigation
+	        $.each(model.connections.connections.connection, function(i,connection){
+	            $.each(connection.schemas, function(i,schema){
+	            	$data_list.append('<optgroup label="'+schema['@schemaname']+'">');
+	                $.each(schema.cubes, function(i,cube){
+	                    if(cube.length == undefined)
+	                        cube = [cube];
+	                    $.each(cube, function(i,item){
+	                        $("<option />")
+	                        .attr({
+	                            'value': connection['@connectionid']
+	                        })
+	                        .data({
+	                            'schema': schema['@schemaname'],
+	                            'cube': item['@cubename']
+	                        })
+	                        .text(item['@cubename'])
+	                        .appendTo($data_list);
+	                    });
+	                });
+	                $data_list.append('</optgroup>');
+	            });
+	        });
+        
+	        // Cache the navigation
+	        view.navigation = $data_list.html();
+	        
+	        // Show the window (hidden first time)
+	        $("#header").show();
+	        $("#tab_content").show();
+	        
+	        // Free up UI
+	        view.free();
+        } else {
+        	// Fill the navigation list with the cached copy of the navigation
+        	$data_list.html(view.navigation);
+        }
+        
+        $data_list.change(function() {
+            model.new_query($data_list.find("option:selected"));
         });
+    },
+    
+    new_tab: function() {
+    	// Increment the number rendered tabs
+    	view.rendered_tabs++;
+    	var tab_id = 'query' + view.rendered_tabs;
+    	
+        // Add a new <li/> element to the #tab_list ul (tab)
+        $new_tab_index = $('<li />').addClass('ui-state-default ui-corner-top');
+        $new_tab_link = $('<a />').attr({
+        		'href': '#' + tab_id,
+        		'rel': 'new'})
+        	.addClass('closable')
+        	.text('Unsaved Query')
+        	.appendTo($new_tab_index);
+        $new_tab_close_button = $('<img />').attr({ 'alt': 'Close tab', 'src': 'images/tab/close.png' })
+        	.click(function() {
+        		view.delete_tab($(this).parent("a").attr('id'));
+        	})
+        	.appendTo($new_tab_link);
+        $new_tab_index.appendTo($('#tab_list ul'));
+                             
+        // Add a new <div/> element to the #tab_content <div/> (tab panel)
+        $new_tab = $('<div />').attr({ 'id': tab_id })
+        	.addClass('tab ui-tabs-panel ui-widget-content ui-corner-bottom ui-tabs-hide')
+        	.appendTo($('#tab_content'));
         
-        $('.data_list').change(function() {
-            model.new_query($(this).find("option:selected"));
-        });
-        
-        $("#header").show();
-        $("#tab_content").show();
-        
-        view.free();
+    	$.get('views/_new_query.html', function(data) {
+    		$new_tab.html(data).show();
+    		view.generate_navigation($new_tab)
+    	});
+    },
+    
+    delete_tab: function(index) {
+    	$(index).remove();
     }
 };
 
