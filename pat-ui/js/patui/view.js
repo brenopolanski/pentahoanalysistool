b15156190090481df95cdf287285f091eea714fb
mark@tiemonster.info <mark@tiemonster.info@de4a0b0a-c842-11dd-a2b5-e5f827957e07>
2010-11-01 20:30:08 +0000

    BROKEN - refactored to work with Tab and TabContainer objects instead of jQuery UI tabs. WORK IN PROGRESS!!!

--- pat-ui/js/patui/view.js
+++ pat-ui/js/patui/view.js
@@ -1,12 +1,114 @@
 /*
+ * Object for storing tab metadata
+ */
+var Tab = function(tab_selector, content_selector) {
+	this.tab = tab_selector;
+	this.content = content_selector;
+	this.data = {};
+};
+
+/*
+ * Container object used for tracking tabs
+ */
+var TabContainer = function(tab_container, content_container) {
+	this.tab_container = tab_container;
+	this.content_container = content_container;
+	this.tabs = new Array();
+	this.selected = 0;
+	
+	/*
+	 * Add a tab
+	 */
+	this.add = function() {
+		// Create the tab itself
+		var new_index = this.tabs.length;
+		var $new_tab = $("<li />").addClass("closable ui-state-default ui-corner-top ui-tabs-selected ui-state-active");
+		var $new_tab_link = $("<a />")
+			.html("Unsaved query (" + new_index + ")")
+			.click(function() {
+				this.select(new_index);
+				})
+			.appendTo($new_tab);
+		var $new_tab_closer = $("<span />")
+			.click(function() {
+				this.remove(new_index);
+				})
+			.appendTo($new_tab);
+		$new_tab.appendTo(this.tab_container);
+		
+		// Create the content portion of the tab
+		$new_tab_content = $('<div />')
+			.addClass("tab ui-tabs-panel ui-widget-content ui-corner-bottom tabs-ui-hide")
+			// FIXME - this is preventing caching atm
+			.load("views/_new_query.html", {"_": Math.floor(Math.random()*1000000)}, function() {
+				view.generate_navigation($new_tab);
+			});
+		$new_tab_content.appendTo(this.content_container);
+		
+		// Register the new tab with the TabContainer
+		this.tabs.push(new Tab($new_tab, $new_tab_content));
+	};
+	
+	/*
+	 * Change the selected tab
+	 */
+	this.select = function(index) {
+		this.content_container.find("div").hide();
+		this.tabs[index].content.show();
+	};
+	
+	/*
+	 * Remove a tab and reclaim memory
+	 */
+	this.remove = function(index) {
+		this.tabs[index].tab.remove();
+		this.tabs[index].container.remove();
+		delete this.tabs[index];
+	};
+	
+	/*
+	 * Completely empty the tab container
+	 * (Used for logout)
+	 */
+	this.clear = function() {
+		for (i = 0; i < this.tabs.length; i++) {
+			if (typeof this.tabs[i] != 'undefined') {
+				this.remove[i];
+			}
+		}
+	};
+	
+	/*
+	 * Determine whether or not the TabContainer is empty
+	 */
+	this.is_empty = function() {
+		// If the array is uninitialized, obviously return true
+		if (this.tabs.length == 0)
+			return true;
+		
+		// Check to see if there are any active tabs
+		for (i = 0; i < this.tabs.length; i++) {
+			if (typeof this.tabs[i] != 'undefined') {
+				// An active tab still exists, return false
+				return false;
+			}
+		}
+		
+		// If not, return true
+		return true;
+	};
+};
+
+
+/*
  * This is the view for PATui. This will handle the drawing of the UI.
  */
 
 var view = {	
 	/*
-	 * The number of tabs that have been rendered, for naming new tabs
+	 * Tabs container
 	 */
-	rendered_tabs: 0,
+	tabs: new TabContainer($("#tabs"), $('#tab_panel')),
 	
 	/*
      * This is where you're going to put your initial UI drawing
@@ -20,18 +122,26 @@ var view = {
 		// Blank everything out until the user logs in
 		view.logout();
 	},
+	
+	/*
+     * Prepare the screen for user input
+     */
+	login: function() {
+		// Hide the layout
+		$("#header").show();
+		$("#tab_panel").show();
+	},
     
 	/*
-     * Clear credentials and clear the screen
+     * Clear the screen
      */
 	logout: function() {
 		// Remove all tabs
-		$('#tab_list ul li').remove();
-		$('#tab_content div').remove();
+		this.tabs.clear();
     	
 		// Hide the layout
 		$("#header").hide();
-		$("#tab_content").hide();
+		$("#tab_panel").hide();
 	},
 	
 	/*
@@ -61,9 +171,12 @@ var view = {
     
 	/*
      * Load data list with schema and cubes from the PAT server
+     * FIXME - store data in tab object somehow
      */
 	generate_navigation: function($tab) {
 		$data_list = $tab.find('.data_list');
+		
+		// TODO - Cache HTML itself
     	
 		// Iterate over connections and populate navigation
 		$.each(model.connections.connections.connection, function(i,connection){
@@ -91,9 +204,7 @@ var view = {
         
 		// Show the window (hidden first time)
 		$("#header").show();
-		$("#tab_content").show();
-        
-		view.resize_tabs();
+		$("#tab_panel").show();
         
 		$data_list.change(function() {
 			model.new_query($tab, $data_list.find("option:selected"));
@@ -101,109 +212,7 @@ var view = {
 	},
     
 	new_tab: function() {
-		// Increment the number rendered tabs
-		view.rendered_tabs++;
-
-		var tab_id = 'query' + view.rendered_tabs;
-
-		// Add a new <li/> element to the #tab_list ul (tab)
-		$new_tab_index = $('<li />').addClass('ui-state-default ui-corner-top');
-		$new_tab_link = $('<a />').attr({
-			'href': '#' + tab_id,
-			'rel': 'new'
-		})
-		.addClass('closable')
-		.text('Unsaved Query')
-		.appendTo($new_tab_index);
-		$new_tab_close_button = $('<span />').html('&nbsp;')
-		.click(function() {
-			/* NEW - Start */
-			var index = $('#tab_list').tabs('option', 'selected');
-			view.delete_tab(index);
-			/* NEW - End */
-		})
-		.appendTo($new_tab_link);
-		$new_tab_index.appendTo($('#tab_list ul'));
-                             
-		// Add a new <div/> element to the #tab_content <div/> (tab panel)
-		$new_tab = $('<div />').attr({
-			'id': tab_id
-		})
-		.addClass('tab ui-tabs-panel ui-widget-content ui-corner-bottom ui-tabs-hide')
-		.appendTo($('#tab_content'));
-        
-		$.get('views/_new_query.html', function(data) {
-			$new_tab.html(data); //.show();
-			view.generate_navigation($new_tab)
-		});
-	},
-    
-	delete_tab: function(index) {
-		/* NEW - Start */
-		$('#tab_list').tabs("remove", index);
-	//$('#tab_list ul').append('<li></li>');
-	/* NEW - End */
-	},
-    
-	resize_tabs: function() {
-		var $tabs, outer_layout, tab_layout_options;
-
-		// Resizes the tab panel when 'show' event is triggered
-		function resize_tab_panel_layout (ui) {
-			var tabIndex = $tabs.tabs("option", "selected"),
-			$tab_panel = $( "#query"+ (tabIndex + 1) );
-			if ($tab_panel.data("layoutContainer")) {
-				$tab_panel.layout().resizeAll();
-			} else {
-				$tab_panel.layout(tab_layout_options);
-			}
-			return;
-		}
-
-		// Layout options for the contents inside the tab
-		tab_layout_options = {
-			name:                       'tab_layout_options',
-			resizeWithWindow:           false,
-			north__resizable:           false,
-			north__closable:            false,
-			spacing_open:               4,
-			spacing_closed:             4,
-			center__paneSelector:       ".tab_body",
-			west__paneSelector:         ".sidebar",
-			west__size:                 "250",
-			contentSelector:            ".tab_scroll_content",
-			west__resizable:            true,
-			west__closable:             true
-		};
-
-
-		outer_layout = $("body").layout({
-			name:                   'outer_layout',
-			resizeWithWindowDelay:	250,
-			closable:		false,
-			resizable:		false,
-			spacing_open:    	0,
-			spacing_closed: 	0,
-			center__paneSelector:	"#tab_content",
-			north__paneSelector:	"#header",
-			center__onresize:       resize_tab_panel_layout
-		});
-
-		window.tabs_loading = true;
-
-		$tabs = $("#tab_list");
-		$tabs.tabs({
-			show: function (evt, ui) {
-				if (tabs_loading) {
-					tabs_loading = false;
-					outer_layout.resizeAll();
-				}
-				resize_tab_panel_layout(ui);
-			}
-		});
-        
-		//$tabs.tabs( "enable");
-		$('#tab_list').tabs( "select", $('#tab_list ul li').length - 1 );
+		this.tabs.add();
 	}
 };
 
